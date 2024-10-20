4c431875dc6632d13f97b00aead244ac7e863718
Animator55 <92542799+Animator55@users.noreply.github.com>
2024-10-20 16:26:29 -0300

    added game bases

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,90 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { Place } from "./vite-env"
+import { faBomb, faFlag, faQuestion } from "@fortawesome/free-solid-svg-icons"
+import React from "react"
 
-function App() {
-  const [count, setCount] = useState(0)
+import "./assets/App.css"
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+// diff: number
+const generateMap = (diff: number)=>{ 
+  let placeList: Place[] = []
+
+  for(let i=0; i<(diff*8)*(diff*8); i++){
+    let place: Place = {
+      _id: `${Math.random()}`,
+      number: 0,
+      bomb: false,
+      state: "hide",
+    }
+    placeList.push(place)
+  }
+
+  // addBombs 
+  for(let i=0; i<diff*diff*50; i++){
+    let index = Math.floor(Math.random() * ((diff*8)*(diff*8)+1))
+    while(!placeList[index] || placeList[index].bomb === true) {
+      index = Math.floor(Math.random() * ((diff*8)*(diff*8)+1))
+    }
+    if(placeList[index]) placeList[index].bomb = true
+    ///checkSurrounding 
+    for(let j=1; j<= 8;j++) {
+      let multiply = j < 4 ? -1 : j > 5 ? 1 : 0
+      let add= j === 1 || j === 4 || j === 6 ? -1 : j=== 3 || j === 5 || j === 8 ? +1 : 0
+
+      if((index % (diff*8) === 0 && add === -1)
+      || ((index+1) % (diff*8) === 0 && add === +1)) continue
+      let secIndex = index + (8 * multiply) + add
+      if(placeList[secIndex]) placeList[secIndex].number++
+    }
+  }
+
+  return placeList
 }
 
-export default App
+export default function App() {
+  let diff: number = 1
+  let bombs = diff*diff*10
+  const [gameState, setGameState] = React.useState<boolean>(false)
+  const [currentMap, setCurrentMap] = React.useState<Place[] | undefined>(undefined)
+  const [gameConfig, setGameConfig] = React.useState()
+
+  const viewPlace = (id: string) => {
+    if(currentMap) setCurrentMap(currentMap.map(el => {
+      if (el._id !== id) return el
+      else return { ...el, state: "view" }
+    }))
+  }
+
+  const placeContent: { [key: string]: any } = {
+    "hide" : null , "flag": faFlag, "question" : faQuestion
+  }
+
+  React.useEffect(()=>{
+    if(currentMap === undefined) setCurrentMap(generateMap(diff))
+  })
+
+  const colorScale = ["",
+    "#548CCD", "#65BFA7", "#7EB753", "#E5D741", "#F2AF44", "#FA8343", "#FC5845", "#FF457D"
+  ]
+
+  return <main>
+    <section className="map"
+      style={{gridTemplateColumns: `repeat(${(diff*8)}, 50px)`}}
+    >
+      {currentMap && currentMap.map((el, i) => {
+        return <button
+          key={Math.random()}
+          onClick={() => { viewPlace(el._id) }}
+          style={{backgroundColor:el.bomb ? "" :  colorScale[el.number]}}
+        >
+          {
+          // el.state === "view" ?
+            el.bomb ? <FontAwesomeIcon icon={faBomb} /> :el.number
+            // : placeContent[el.state]
+          }
+        </button>
+      })}
+    </section>
+  </main>
+
+}
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="vite/client" />
+
+
+
+export type Place = {
+    _id: string
+    number: 0 | 1 | 2 | 3| 4 | 5 | 6| 7 | 8 | 9
+    bomb: boolean
+    state: "view" | "hide" | "flag" | "question"
+}
